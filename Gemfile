c11f0bd5752806bc499b62d0a92a2566641b6538
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2018-06-13 17:38:10 +0300

    #2 Добавлена поддержка библиотеки `sequel`

--- Gemfile
+++ Gemfile
@@ -4,13 +4,18 @@ source 'http://repo.it2.vm/repository/gem-group'
 
 gem 'activesupport'
 gem 'dotenv'
+gem 'rake'
+gem 'sequel'
+gem 'sequel_pg'
 
 group :development, :test do
   gem 'awesome_print'
+  gem 'factory_bot'
   gem 'rspec'
   gem 'yard'
 end
 
 group :test do
+  gem 'database_cleaner'
   gem 'rubocop'
 end
---
+++ config/initializers/02_sequel.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# Настройка Sequel
+
+require 'sequel'
+require 'erb'
+require 'yaml'
+
+# Подключение расширений Sequel:
+# 1) поддержка миграций;
+Sequel.extension :migration
+# 2) расширения базовых классов
+Sequel.extension :core_extensions
+
+# Инициализация подключения к базе данных
+# Загрузка настройки базы данных
+erb = IO.read("#{Cab.root}/config/database.yml")
+yaml = ERB.new(erb).result
+options = YAML.safe_load(yaml, [], [], true)
+# Подключение
+db = Sequel.connect(options[Cab.env])
+# Журнал событий
+db.loggers << Cab.logger unless Cab.production?
+# Установка того, на каком уровне журнала событий происходит отображение
+# SQL-запросов
+db.sql_log_level = :debug
+# Подключение поддержки перечислимых типов Postgres. Важно, что подключение
+# расширения pg_enum идёт после подключения расширения Sequel migration.
+db.extension :pg_enum
+
+# Настройка моделей
+# Установка базу данных для моделей. Через свойство Sequel::Model.db в
+# дальнейшем можно обращаться к базе данных.
+Sequel::Model.db = db
+# Подключение поддержки генерации исключений
+Sequel::Model.raise_on_save_failure = true
+Sequel::Model.raise_on_typecast_failure = true
+
+# Загрузка моделей
+Cab.need 'models/*.rb'
+
+# Отключение, необходимое для использования Puma. Без отключения процессы Puma
+# используют одно и то же подключение, определённое в родительском процессе,
+# что приводит к странным ошибкам. После отключения Sequel автоматически
+# подключается к базе данных индивидуально для каждого дочернего процесса.
+db.disconnect
