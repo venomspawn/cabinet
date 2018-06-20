411a4f08fd908ae7c19e3821db937cb6a02c949c
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2018-06-20 11:38:27 +0300

    #7 Добавлено действие извлечения информации о заявителе

---
+++ spec/helpers/cab/actions/applicants/show/spec_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Cab
+  need 'actions/applicants/show/result_schema'
+
+  module Actions
+    module Applicants
+      class Show
+        # Вспомогательный модуль, предназначенный для подключения к тестам
+        # действия содержащего класса
+        module SpecHelper
+          # Возвращает JSON-схему результирующего действия
+          # @return [Object]
+          #   JSON-схема результирующего действия
+          def schema
+            RESULT_SCHEMA
+          end
+        end
+      end
+    end
+  end
+end
