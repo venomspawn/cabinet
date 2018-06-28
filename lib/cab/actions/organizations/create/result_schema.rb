2f639598036e737787ce2f6ae4dd5a44efad10e9
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2018-06-28 09:14:08 +0300

    #16 Изменён REST API метод создания записи ЮЛ

--- lib/cab/actions/organizations/create/result_schema.rb
+++ lib/cab/actions/organizations/create/result_schema.rb
@@ -8,55 +8,12 @@ module Cab
         RESULT_SCHEMA = {
           type: :object,
           properties: {
-            client_type: {
-              type: :string,
-              enum: %w[organization]
-            },
             id: {
               type: :string
-            },
-            full_name: {
-              type: :string
-            },
-            sokr_name: {
-              type: %i[null string]
-            },
-            director: {
-              type: :string
-            },
-            registration_date: {
-              type: :string
-            },
-            inn: {
-              type: :string
-            },
-            kpp: {
-              type: :string
-            },
-            ogrn: {
-              type: :string
-            },
-            legal_address: {
-              type: %i[string number object array boolean]
-            },
-            actual_address: {
-            },
-            bank_details: {
             }
           },
           required: %i[
-            client_type
             id
-            full_name
-            sokr_name
-            director
-            registration_date
-            inn
-            kpp
-            ogrn
-            legal_address
-            actual_address
-            bank_details
           ],
           additionalProperties: false
         }.freeze
